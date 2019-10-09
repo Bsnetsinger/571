38e4d265b020e77b1696412dd576ded9600cd9ad
Michael Fox <Michaelkfox100@gmail.com>
2019-10-09 12:32:02 -0700

    fibLoop
    CPU Usage File

---
+++ fibLoop.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int sum = 1; 
+    int current = 1, prev = 0;
+
+    while(1){
+        while(current < 200)
+        {
+            sum = current + prev;
+            prev = current;
+            current = sum;
+        }
+
+    }
+
+}
+
