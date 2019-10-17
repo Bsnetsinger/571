4d30c5fa2c606c1511b0aace329818087076af7b
Bradley <bradleysnetsinger@gmail.com>
2019-10-16 19:43:21 -0700

    time update 3

--- time.sh
+++ time.sh
@@ -2,5 +2,5 @@
 gcc bubbleSort.c -o bubbleSort;
 start=$(date +%s.%N);
 ./bubbleSort;
-dur=$(echo "$(date +%s.%N) - $start" \ bc);
+dur=$(echo "$(date +%s.%N) - $start" | bc);
 echo "$dur" >> /home/pi/Desktop/data.txt;
