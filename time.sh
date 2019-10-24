2cd948ebf2628c93996a934fde5c354a97837e09
Bradley <bradleysnetsinger@gmail.com>
2019-10-23 20:32:00 -0700

    bubble and fib in script

--- time.sh
+++ time.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
+
 gcc fibLoop.c -o fibLoop;
+gcc bubbleSort.c -o bubbleSort;
+
+for i in {-20..19}
+do 
+for j in {-20..19}
 start=$(date +%s.%N);
-./fibLoop;
+nice -n i ./fibLoop &
+nice -n j ./bubbleSort;
 dur=$(echo "$(date +%s.%N) - $start" | bc);
-echo "$dur" >> /home/pi/Desktop/data.txt;
+echo "$i $j $dur" >> /home/pi/Desktop/data.txt;
+done
+done
+
+
