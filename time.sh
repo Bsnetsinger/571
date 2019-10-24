7931a2efd34fafb1ae072585a285f93b2cedb1f2
Bradley <bradleysnetsinger@gmail.com>
2019-10-23 20:38:47 -0700

    error

--- time.sh
+++ time.sh
@@ -5,13 +5,13 @@ gcc bubbleSort.c -o bubbleSort;
 
 for i in {-20..19}
 do 
-for j in {-20..19}
-start=$(date +%s.%N);
-nice -n i ./fibLoop &
-nice -n j ./bubbleSort;
-dur=$(echo "$(date +%s.%N) - $start" | bc);
-echo "$i $j $dur" >> /home/pi/Desktop/data.txt;
-done
+    for j in {-20..19}
+        start=$(date +%s.%N);
+        nice -n $i ./fibLoop &
+        nice -n $j ./bubbleSort;
+        dur=$(echo "$(date +%s.%N) - $start" | bc);
+        echo "$i $j $dur" >> /home/pi/Desktop/data.txt;
+    done
 done
 
 
