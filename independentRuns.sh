bfa01bce48a3d4d4ccb8f5385564a451ff09a7d5
Michael Fox <Michaelkfox100@gmail.com>
2019-10-28 22:13:58 -0700

    Add fix

--- independentRuns.sh
+++ independentRuns.sh
@@ -16,15 +16,15 @@ do
     start=$(date +%s.%N);
     ./fibLoop;
     dur=$(echo "$(date +%s.%N) - $start" | bc);
-    x=$(echo "$x + $dur" | bc) ;
+    x=$(echo "$x + $dur" | bc);
 done
 
 for j in {0..100}
 do 
     start=$(date +%s.%N);
     ./bubbleSort;
-    dur=$(echo "$(date +%s.%N) - $start" | bc);
-    y=$(echo "$y + $dur" | bc);
+    dur2=$(echo "$(date +%s.%N) - $start" | bc);
+    y=$(echo "$y + $dur2" | bc);
 done
 
 echo "100 Runs of fibLoop: $x \n 100 Runs of bubbleSort: $y" >> /home/pi/Desktop/runTimes.txt;
