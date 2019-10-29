2bea0ab8d67dda68e69c6608b33bdc5ea31ea850
Michael Fox <Michaelkfox100@gmail.com>
2019-10-28 21:58:32 -0700

    Add bug fix

--- independentRuns.sh
+++ independentRuns.sh
@@ -8,21 +8,20 @@ temp=$(echo "$(date +%s.%N) - $start" | bc);
 
 gcc fibLoop.c -o fibLoop;
 gcc bubbleSort.c -o bubbleSort;
-x=0.000;
-y=0.000;
+
 
 for i in {0..100}
 do 
     start=$(date +%s.%N);
     ./fibLoop;
     dur=$(echo "$(date +%s.%N) - $start" | bc);
-    x=$($x+$dur);
+    x=$(($x+$dur));
 done
 for j in {0..100}
 do 
     start=$(date +%s.%N);
     ./bubbleSort;
     dur=$(echo "$(date +%s.%N) - $start" | bc);
-    y=$($y+$dur);
+    y=$(($y+$dur));
 done
 echo "100 Runs of fibLoop: $x \n 100 Runs of bubbleSort: $y" >> /home/pi/Desktop/runTimes.txt;
