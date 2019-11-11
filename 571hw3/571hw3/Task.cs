5afeff1ca3d2a81a06d8bc4081079fc8599ad96b
Michael <Michaelkfox100@gmail.com>
2019-11-11 14:19:08 -0800

    Task Execute and current time function

--- 571hw3/571hw3/Task.cs
+++ 571hw3/571hw3/Task.cs
@@ -5,6 +5,9 @@ public class Task
     public string name;
     public int period;
     public int wcet1188, wcet918, wcet648, wcet384;
+    public bool available;
+    public int remainingTime;
+    public int periodCount;
 
     public Task(string LineIn)
     {
@@ -15,7 +18,28 @@ public class Task
         wcet918 = Convert.ToInt32(values[3]);
         wcet648 = Convert.ToInt32(values[4]);
         wcet384 = Convert.ToInt32(values[5]);
+        available = true;
+        remainingTime = wcet1188;
+        periodCount = 0;
     }
-
-    
+    public void CurrentTime(int Time)
+    {
+        if ((periodCount + 1) * period == Time) //Begin new period, task re-enters the system
+        {
+            periodCount++;
+            remainingTime = wcet1188; //Reset execution time
+        }
+               
+        if (remainingTime > 0 && Time >= (periodCount * period)) //Has not been fully executed and task is in the system
+            available = true;
+        else
+            available = false;
+    }
+    public void Execute()
+    {
+        if (remainingTime > 0)
+            remainingTime--;
+        else
+            Console.WriteLine("Error: Attempt to execute completed task.");
+    }
 }
