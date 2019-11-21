b78ff31ccc0ee5e8720698e40cea46785a1c6381
Michael Fox <Michaelkfox100@gmail.com>
2019-11-20 21:00:13 -0800

    Auto stash before merge of "master" and "origin/master"

--- 571hw3/571hw3/Program.cs
+++ 571hw3/571hw3/Program.cs
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -416,3 +417,36 @@ namespace _571hw3
 }
 
 
+=======
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _571hw3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Input;
+            string Type;
+            string Energy;
+
+            Console.WriteLine("Input file name: ");
+            Input = Console.ReadLine();
+            Console.WriteLine("Scheduling type: ");
+            Type = Console.ReadLine();
+            Console.WriteLine("EE or N/A: ");
+            Energy = Console.ReadLine();
+
+            Console.WriteLine("{0} , {1}, {2}", Input, Type, Energy);
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
+>>>>>>> Stashed changes
--- bubbleSort.c
+++ bubbleSort.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main()
 {
+	time_t start = time(NULL);
   
 
     int A[1000];
@@ -17,18 +19,25 @@ int main()
 
     int size = 1000;
 
-	    for(int i=0; i<size; i++)
-	    {
-		    for(int j=0; j<size-1; j++)
-		    {
-			    if( A[j] > A[j+1] )
-		    	{
-				    int temp = A[j];
-				    A[j] = A[j+1];
-				    A[j+1] = temp;
-			    }
+	for(int i=0; i<size; i++)
+	{
+		for(int j=0; j<size-1; j++)
+		{
+			if( A[j] > A[j+1] )
+			{
+				int temp = A[j];
+				A[j] = A[j+1];
+				A[j+1] = temp;
+			}
 		}
-	    }
+	}
+
+	time_t end = time(NULL);
+
+	int runTime = end - start;
+
+	file = fopen("/home/pi/Desktop/bubbleSortTimes.txt", "a");
+	fprintf(file, "%d", runTime);
 
     return 0;
 
