fe21c1d4781fb9743cfb558e9b859923849c2581
Bradley <bradleysnetsinger@gmail.com>
2019-12-05 13:12:12 -0800

    time2

---
+++ time2.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+declare -i x;
+declare -i y;
+
+start=$(date +%s.%N);
+sleep 0.851111111;
+temp=$(echo "$(date +%s.%N) - $start" | bc);
+
+gcc bubbleSort.c -o bubbleSort; 
+
+./bubbleSort 
+
+top -b -m 1 > /home/pi/Desktop/data.txt;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# for i in {-20..19}
+# do 
+#     for j in {-20..19}
+#     do
+#         start=$(date +%s.%N);
+#         sudo nice -n $i ./fibLoop &
+#         sudo nice -n $j ./bubbleSort &
+#         wait;
+#         dur=$(echo "$(date +%s.%N) - $start" | bc);
+#         echo "$i $j $dur" >> /home/pi/Desktop/data.txt;
+#         if (( $(echo "$temp > $dur" |bc -l) ))
+#         then
+#             temp=$dur;
+#             x=$i;
+#             y=$j;
+#         fi
+#     done
+# done
+
+# echo "Best: $x $y $temp" >> /home/pi/Desktop/data.txt;
