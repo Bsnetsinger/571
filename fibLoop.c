aa7436afb47d02098672070a754da7e2d2dabc4f
Bradley <bradleysnetsinger@gmail.com>
2019-12-06 20:07:41 -0800

    upfdate

--- fibLoop.c
+++ fibLoop.c
@@ -10,28 +10,31 @@ int main()
 	
 	start_t = clock();
 
+    int count = 0;
     int sum = 1; 
     int current = 1, prev = 0;
 
-    while(1){
-        while(current < 2000000000)
+    while(count < 1000000){
+        while(current < 200000)
         {
             sum = current + prev;
             prev = current;
             current = sum;
         }
-
-        end_t = clock();
-
-	    total_t = ((double)(end_t - start_t)) / CLOCKS_PER_SEC; 
-
-         FILE * f;
-	    f = fopen("/home/pi/Desktop/fibLoopTimes.txt", "a");
-	    fprintf(f, "%f\n", total_t);
-
-        return 0;
-
+        sum = 1;
+        current = 1;
+        prev = 0;
+        count += 1;
     }
+    end_t = clock();
+
+	total_t = ((double)(end_t - start_t)) / CLOCKS_PER_SEC; 
+
+    FILE * f;
+	f = fopen("/home/pi/Desktop/fibLoopTimes.txt", "a");
+	fprintf(f, "%f\n", total_t);
+
+    return 0;
 
 }
 
