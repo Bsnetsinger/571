8771a3857328bec11e633ae3852fe6e74229182e
Bradley <bradleysnetsinger@gmail.com>
2019-12-10 21:51:12 -0800

    equal one fix

--- time2.sh
+++ time2.sh
@@ -32,23 +32,23 @@ for i in {0..100}
 do 
     top -b -n 1 > /home/pi/Desktop/data.txt;
 
-    if [ "$flag1" ] ; then
+    if [ "$flag1" -eq "1" ] ; then
     cat /home/pi/Desktop/data.txt | grep $PID1 | cut -c 49-53 | nl >> /home/pi/Desktop/cpu1.txt;
     cpu1="$(cat /home/pi/Desktop/data.txt | grep $PID1 | cut -c 49-53)";
     fi
     
-    if [ "$flag2" ] ; then
+    if [ "$flag2" -eq "1" ] ; then
     cat /home/pi/Desktop/data.txt | grep $PID2 | cut -c 49-53 | nl >> /home/pi/Desktop/cpu2.txt;
     cpu2="$(cat /home/pi/Desktop/data.txt | grep $PID2 | cut -c 49-53)";
     fi
     
-    if [ -z "$cpu1" ] && [ "$flag1" ] ; then
+    if [ -z "$cpu1" ] && [ "$flag1" -eq "1" ] ; then
         dur1=$(echo "$(date +%s.%N) - $start" | bc);
         flag1=0;
         stop=0;
     fi
 
-    if [ -z "$cpu2" ] && [ "$flag2" ] ; then
+    if [ -z "$cpu2" ] && [ "$flag2" -eq "1" ] ; then
         dur2=$(echo "$(date +%s.%N) - $start" | bc);
         flag2=0;
         stop=0;
@@ -58,7 +58,7 @@ do
         break;
     fi
 
-    if [ "$stop" ] ; then
+    if [ "$stop" -eq "1" ] ; then
         if(( $(echo "$cpu1 > $cpu2" |bc -l) ));then
             nice2=$(($nice2 - 1));
             sudo renice $nice2 $PID2;
